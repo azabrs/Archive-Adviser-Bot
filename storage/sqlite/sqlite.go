acfbb372e2f59e7aea014ef8a1234cc539b145d3
Apuha <Apuha@mail.ru>
2024-03-05 00:56:40 +0300

    add sqllite storage

---
+++ storage/sqlite/sqlite.go
@@ -0,0 +1,83 @@
+package sqlite
+
+import (
+	"Archive-Adviser-Bot/storage"
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type sqlite struct{
+	db *sql.DB
+}
+
+func New(path string) (*sqlite, error){
+	db, err := sql.Open("sqllite3", path)
+	if err != nil {
+		return nil, fmt.Errorf("can't open database: %w", err)
+	}
+
+	err = db.Ping()
+	if err != nil{
+		return nil, fmt.Errorf("can't connect to database: %w", err) 
+	}
+	return &sqlite{ db: db}, nil
+}
+
+func (sq *sqlite)Save(ctx context.Context, p *storage.Page) error{
+	q := `INSERT INTO pages (url, user_name) VALUES (?, ?)`
+	if _, err := sq.db.ExecContext(ctx, q, p.URL, p.UserName); err != nil {
+		return fmt.Errorf("can't save page: %w", err)
+	}
+	return nil
+}
+
+func (s *sqlite) PickRandom(ctx context.Context, userName string) (*storage.Page, error) {
+	q := `SELECT url FROM pages WHERE user_name = ? ORDER BY RANDOM() LIMIT 1`
+
+	var url string
+
+	err := s.db.QueryRowContext(ctx, q, userName).Scan(&url)
+	if err == sql.ErrNoRows {
+		return nil, storage.ErrNoSavedPages
+	}
+	if err != nil {
+		return nil, fmt.Errorf("can't pick random page: %w", err)
+	}
+
+	return &storage.Page{
+		URL:      url,
+		UserName: userName,
+	}, nil
+}
+
+func (s *sqlite) Remove(ctx context.Context, page *storage.Page) error {
+	q := `DELETE FROM pages WHERE url = ? AND user_name = ?`
+	if _, err := s.db.ExecContext(ctx, q, page.URL, page.UserName); err != nil {
+		return fmt.Errorf("can't remove page: %w", err)
+	}
+
+	return nil
+}
+
+
+func (s *sqlite) IsExists(ctx context.Context, page *storage.Page) (bool, error) {
+	q := `SELECT COUNT(*) FROM pages WHERE url = ? AND user_name = ?`
+
+	var count int
+
+	if err := s.db.QueryRowContext(ctx, q, page.URL, page.UserName).Scan(&count); err != nil {
+		return false, fmt.Errorf("can't check if page exists: %w", err)
+	}
+
+	return count > 0, nil
+}
+
+func (sq *sqlite) INIT(ctx context.Context) error{
+	q := `CREATE TABLE IF NOT EXISTS pages (url TEXT, user_name TEXT)`
+	_, err := sq.db.ExecContext(ctx, q)
+	if err != nil {
+		return fmt.Errorf("can't create table: %w", err)
+	}
+	return nil
+}
